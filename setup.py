1989d4700fd049b48efa9d1af0f2da5d1ed2a1db
Waseju <jwgithub@mailbox.org>
2021-11-29 16:29:56 +0100

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -74,6 +74,6 @@ setup(
     setup_requires=setup_requirements,
     tests_require=test_requirements,
     url='https://github.com/waseju/rtsstat',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
